5c6daf62e1f7c5e7e234dcc080421cd4307738a1
Konrad Ciepluch <konrad190008@wp.pl>
2021-08-11 07:31:02 +0200

    Styles fixed

--- src/components/Example/Button/Button.styles.js
+++ src/components/Example/Button/Button.styles.js
@@ -1,27 +1,31 @@
 import styled from "styled-components";
-import { getFontSize } from "@root/styles/utils";
+import { getFontSize, getColor } from "@root/styles/utils";
 
 
 export const BigButton = styled.button`
-    background-color: ${({theme, disabled}) => disabled ? `#B9DBF8` : theme.color.ikksBlue};
+    background-color: ${getColor('ikssBlue')};
     height: 48px;
     padding: ${({icon}) => icon ? `12px 24px 12px 18px` : `14px 24px`} ;
     border-radius: 26px;
     font-weight: 700;
     line-height: 20px;
-    color: ${({theme}) => theme.color.white};
+    color: ${getColor('white')};
     font-size: ${getFontSize('body')};
     border: none;
     display: flex;
     justify-content: center;
     align-items: center;
 
+    &:disabled {
+        background-color: #B9DBF8;
+    }
+
+    &:active {
+        background-color: #167CD2;
+    }
+
     &:hover {
-        background-color: ${({isPressed, disabled}) => {
-            if(disabled) return `#B9DBF8`
-            if(isPressed) return `#167CD2`
-            return `#2F95EB`
-        } };
+        background-color: #2F95EB;
         box-shadow: 0px 4px 8px rgba(24, 137, 233, 0.15);
     }
 
@@ -32,35 +36,40 @@ export const BigButton = styled.button`
 
 export const SecondaryBigButton = styled(BigButton)`
     border: 2px solid;
-    border-color: ${({theme, disabled}) => disabled ? `#BABEC7` : theme.color.navy};
-    background-color: ${({theme}) => theme.color.white};
-    color: ${({theme, disabled}) => disabled ? `#BABEC7` : theme.color.navy};
+    border-color: ${getColor('navy')};
+    background-color: ${getColor('white')};
+    color: ${getColor('navy')};
+
+    &:disabled {
+        color: #BABEC7;
+        border-color: #BABEC7;
+    }
+
+    &:active {
+        border-color: #535E75;
+        color: #535E75;
+    }
 
     &:hover {
-        border-color: ${({isPressed, disabled}) => {
-            if(disabled) return `#BABEC7`
-            if(isPressed) return `#535E75`
-            return `#3C4862`
-        } };
-        color:  ${({isPressed, disabled}) => {
-            if(disabled) return `#BABEC7`
-            if(isPressed) return `#535E75`
-            return `#3C4862`
-        } };
-        background-color: ${({theme}) => theme.color.white};
+        border-color: #3C4862;
+        color: #3C4862;
         box-shadow: none;
     }
 
     svg {
-        color: ${({theme, disabled}) => disabled ? `#BABEC7` : theme.color.navy};
+        color: ${getColor('navy')};
+    }
+
+    &:disabled svg {
+        color: #BABEC7;
+    }
+
+    &:active svg {
+        color: #535E75;
     }
 
     &:hover svg {
-        color:  ${({isPressed, disabled}) => {
-            if(disabled) return `#BABEC7`
-            if(isPressed) return `#535E75`
-            return `#3C4862`
-        } };
+        color: #3C4862;
     }
 `;
 
