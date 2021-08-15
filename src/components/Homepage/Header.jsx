c1864c1c9c971a8939377f92083f2091cab3d282
Nimavein <michalzygiel00@gmail.com>
2021-08-15 14:33:25 +0200

    nextjs error remove from header component

--- src/components/Homepage/Header.jsx
+++ src/components/Homepage/Header.jsx
@@ -1,28 +1,17 @@
 import Image from 'next/image';
 import PropTypes from 'prop-types';
-import Error from 'next/error';
 import { BodyText, H1 } from '../typography/Typography';
 import ImageWrapper from '../wrappers/ImageWrapper';
 
-const Header = ({ title, text, image, socialLinks }) => {
-  const isFetched = title && text && image && socialLinks;
-
-  return (
-    <div>
-      {isFetched ? (
-        <>
-          <H1>{title}</H1>
-          <BodyText>{text}</BodyText>
-          <ImageWrapper>
-            <Image width="808px" height="505px" src={`https:${image}`} alt="top-section-image" />
-          </ImageWrapper>
-        </>
-      ) : (
-        <Error statusCode="Data from API is missing" />
-      )}
-    </div>
-  );
-};
+const Header = ({ title, text, image, socialLinks }) => (
+  <>
+    <H1>{title}</H1>
+    <BodyText>{text}</BodyText>
+    <ImageWrapper>
+      <Image width="808px" height="505px" src={`https:${image}`} alt="top-section-image" />
+    </ImageWrapper>
+  </>
+);
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
