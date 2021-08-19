4c6d7ae6fa05b1a31623132c31c266ab6f9ac499
TechLounge777 <msblanche777@gmail.com>
2021-08-19 17:20:07 +0200

    Added Header

---
+++ src/components/HomePage/Header/Header.styles.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import { getColor } from "@root/styles/utils";
+import { H1, H5, BodyText } from '@root/components/typography/Typography';
+import { SecondaryBigButton } from '@root/components/Button/Button.styles';
+
+export const HeroSection = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    position: relative;
+    
+`;
+
+export const MainContent = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1440px;
+    
+`;
+
+export const LeftBlock = styled.div`
+ 
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 50%;
+    
+`;
+
+export const Vid = styled.div`
+
+width: 808px;
+height: 505px;
+margin-top: 0px;
+margin-right:0px;
+`;
+
+export const HeaderSocialMediaWrapper= styled.div`
+ 
+display: flex;
+justify-content: space-around;
+align-items: center;
+width: 1038px;
+height: 137px;
+margin-right:0px;
+margin-left: 402px;
+margin-top: 0x;
+background-color: #ffffff;
+box-shadow: 3.38443px 55.8976px 80px rgba(97, 121, 139, 0.07), 1.71337px 28.2982px 34.875px rgba(97, 121, 139, 0.04725), 0.676885px 11.1795px 13px rgba(97, 121, 139, 0.035), 0.148069px 2.44552px 4.625px rgba(97, 121, 139, 0.02275);
+border-radius: 16px 0px 0px 16px;
+margin-bottom: 32px;
+`;
+
+export const Socials = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 835px;
+    height: 120px;
+    margin-left: 56px;
+    margin-right: 120px;
+    margin-top: 44px;
+    margin-bottom: 45px;
+    
+    a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        cursor: pointer;
+    }
+`;
+
+export const HeroHeading = styled(H1)`
+margin-top: 77px;
+margin-left: 120px;
+`;
+
+export const HeroLinks = styled(H5)`
+margin-left: 14px;
+color: ${getColor('navy')};
+`;
+
+
+export const HeroBodyText = styled(BodyText)`
+margin-top: 32px;
+margin-left: 120px;
+width: 384px;
+`;
+
+export const HeroSecondaryBigButton = styled(SecondaryBigButton)`
+margin-top: 32px;
+margin-left: 120px;
+margin-bottom: 42px;
+`;
+
+
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,54 @@
-import PrimaryLogo from '@root/components/logos/PrimaryLogo';
-import LogoNoBackground from '@root/components/logos/LogoNoBackground';
-import LogoNoBackgroundWhite from '@root/components/logos/LogoNoBackgroundWhite';
-import { H1, BodyText } from '@root/components/typography/Typography';
-import { getColor } from '@root/styles/utils';
+import PropTypes from 'prop-types';
+import fetchContentfulApi from '@root/api/ContentfulClient';
+import Header from '@root/components/HomePage/Header/Header';
+import getSocialMedias from '@root/handlers/getSocialMedias';
 
+import { findApiElementByIdentifier, findAssetByTitle } from '@root/handlers/findApiElement';
 
+const Home = ({ homeApiElements, homeApiAssets, commonApiElements }) => {
+  const homeTopSection = findApiElementByIdentifier(homeApiElements, 'homepage-top-section');
+  const topSectionVideoUrl = findAssetByTitle(homeApiAssets, 'na strone ikss').fields.file.url;
+  const socialMedias = getSocialMedias(commonApiElements);
+  const {
+    fields: {
+      text1: { content },
+      title,
+    },
+  } = homeTopSection;
 
-const Home = () => (
-  <div>
-    <PrimaryLogo margin="20px 0 0 120px" height="49px" width="78px" />
-    <LogoNoBackground margin="20px 0 0 120px" height="49px" width="78px" />
-    <LogoNoBackgroundWhite margin="20px 0 0 120px" height="49px" width="78px" />
-    <H1 color={getColor('steel')} margin="20px 0 0 40px">
-      Example h1 with adjusted color and margin
-    </H1>
-    <BodyText>Basic body text</BodyText>
-    
-    
-  </div>
-  
-     
-);
+  const [firstSection] = content;
+  const topSectionBodyText = firstSection.content[0].value;
+
+  return (
+    <>
+      <Header
+        headerTitle={title}
+        text={topSectionBodyText}
+        video={topSectionVideoUrl}
+        socialMedias={socialMedias}
+        
+      />
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  const homeApiElements = await fetchContentfulApi.getBasicContent('homepage');
+  const homeApiAssets = await fetchContentfulApi.getBasicContentAssets('homepage');
+  const commonApiElements = await fetchContentfulApi.getBasicContent('common');
+  return {
+    props: {
+      homeApiElements,
+      homeApiAssets,
+      commonApiElements,
+    },
+  };
+};
+
+Home.propTypes = {
+  homeApiElements: PropTypes.arrayOf(PropTypes.object).isRequired,
+  commonApiElements: PropTypes.arrayOf(PropTypes.object).isRequired,
+  homeApiAssets: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Home;
