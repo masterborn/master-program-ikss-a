00a76427efe1f466a5636266dd6c49c8e09f6727
TechLounge777 <msblanche777@gmail.com>
2021-08-19 21:20:20 +0200

    updated files

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import PropTypes from 'prop-types';
 import fetchContentfulApi from '@root/api/ContentfulClient';
 import Header from '@root/components/HomePage/Header/Header';
