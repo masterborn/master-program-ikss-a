9f79902a5505565a10db4355234a8f35e9fb30b7
Nimavein <michalzygiel00@gmail.com>
2021-08-22 14:08:12 +0200

    put back Hydrate inside _app.jsx file

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -24,15 +24,18 @@ const App = (props) => {
         <title>App Name</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.png" />
-        <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600;700;800;900&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <ThemeProvider theme={theme}>
         <QueryClientProvider client={queryClientRef.current}>
-          
+          <Hydrate state={pageProps.dehydratedState}>
             <Layout commonApiElements={commonApiElements}>
               <Component {...pageProps} />
             </Layout>
-          
+          </Hydrate>
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
         <GlobalStyles />
