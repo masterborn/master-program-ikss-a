527bddc26769d56888fd6db8e7a2ada53af89426
Adam <147478+AdamKopf@users.noreply.github.com>
2021-08-23 14:49:42 +0200

    Update ContentfulClient.js

--- src/api/ContentfulClient.js
+++ src/api/ContentfulClient.js
@@ -3,44 +3,31 @@ import axios from 'axios';
 const ACCESS_TOKEN = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 const SPACE_ID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
 
-const fetchContentfulApi = {
+const contentfulClient = {
   baseUrl: `https://cdn.contentful.com/spaces/${SPACE_ID}/environments/master/entries?access_token=${ACCESS_TOKEN}&order=-sys.updatedAt&content_type=`,
-
-  async getAssets(contentType) {
-    const response = await axios.get(`${this.baseUrl}${contentType}`);
-    return response.data.includes.Asset;
+  
+  cache: {},
+  
+  async makeRequest(url){
+  
   },
+
   async getItems(contentType) {
-    const response = await axios.get(`${this.baseUrl}${contentType}`);
-    return response.data.items;
+    return this.makeRequest(`${this.baseUrl}${contentType}`);
   },
 
   async getProjects() {
     return this.getItems('projects');
   },
-  async getProjectsAssets() {
-    return this.getAssets('projects');
-  },
   async getBoardMembers() {
     return this.getItems('boardMembers');
-  },
-  async getBoardMembersAssets() {
-    return this.getAssets('boardMembers');
   },
   async getPartnerLogos() {
     return this.getItems('partnerLogos');
   },
-  async getPartnerLogosAssets() {
-    return this.getAssets('partnerLogos');
-  },
   async getBasicContent(page) {
-    const response = await axios.get(`${this.baseUrl}basicContent&fields.page[in]=${page}`);
-    return response.data.items;
-  },
-  async getBasicContentAssets(page) {
-    const response = await axios.get(`${this.baseUrl}basicContent&fields.page[in]=${page}`);
-    return response.data.includes.Asset;
+    return this.makeRequest(`${this.baseUrl}basicContent&fields.page[in]=${page}`);
   },
 };
 
-export default fetchContentfulApi;
+export default contentfulClient;
