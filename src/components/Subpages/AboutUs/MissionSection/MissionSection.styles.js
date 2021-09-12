7d7b40d8491722779cb67289ab6cc894029dd740
TechLounge777 <msblanche777@gmail.com>
2021-09-12 22:13:34 +0200

    adjusted @media to medium

--- src/components/Subpages/AboutUs/MissionSection/MissionSection.styles.js
+++ src/components/Subpages/AboutUs/MissionSection/MissionSection.styles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { H2, BodyText } from '@root/components/typography/Typography';
-import { medias } from '@root/styles/theme';
 import {
   getColor,
   getFontSize,
@@ -9,36 +8,37 @@ import {
   getLineHeight,
 } from '@root/styles/utils';
 
-const { small } = medias;
-
 export const MissionSectionWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  position: relative;
+  margin: 0 auto 0 auto;
+  padding-left: 24px;
+  padding-right: 24px;
   margin-top: 106px;
   margin-bottom: 177px;
-  @media ${small} {
-    margin-top: 80px;
-    margin-bottom: 80px;
+  @media ${({ theme }) => theme.medias.medium} {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
 export const ImageWrapper = styled.div`
+  height: 245px;
   border-radius: 16px;
   overflow: hidden;
-  @media ${small} {
+  @media ${({ theme }) => theme.medias.medium} {
     border-radius: 8px;
-    width: 327px;
-    height: 201px;
   }
 `;
 
 export const StyledHeader = styled(H2)`
   margin-left: 56px;
-  @media ${small} {
-    margin-left: 24px;
-    color: ${getColor('navy')};
+  color: ${getColor('navy')};
+  @media ${({ theme }) => theme.medias.medium} {
+    margin: 0 auto 0 auto;
     font-weight: ${getFontWeight('h4')};
     font-size: ${getFontSize('h4')};
     line-height: ${getLineHeight('h4')};
@@ -47,30 +47,24 @@ export const StyledHeader = styled(H2)`
 `;
 
 export const RightSection = styled.div`
-  width: 658px;
-  height: 128px;
   margin-left: 56px;
-  @media ${small} {
-    width: 327px;
-    height: 196px;
-    margin-bottom: 16px;
-    margin-left: 24px;
-    margin-right: 72px;
+  @media ${({ theme }) => theme.medias.medium} {
+    text-align: center;
+    margin: 32px 0 0 0;
+    padding-left: 24px;
+    padding-right: 24px;
   }
 `;
 
 export const StyledMissionText = styled(BodyText)`
-  width: 658px;
-  height: 120px;
+  max-width: 658px;
   margin-top: 24px;
   margin-left: 56px;
-  @media ${small} {
-    color: ${getColor('steel')};
+  color: ${getColor('steel')};
+  text-align: justify;
+  @media ${({ theme }) => theme.medias.medium} {
     font-size: ${getFontSize('bodySmall')};
-    width: 327px;
-    height: 196px;
-    margin-bottom: 80px;
-    margin-left: 24px;
-    margin-right: 24px;
+    align-content: center;
+    margin: 16px 0 0 0;
   }
 `;
