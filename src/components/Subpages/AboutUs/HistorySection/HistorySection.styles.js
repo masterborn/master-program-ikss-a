ba5c8acfbff0c375ff3d8b3068d913a2e8350441
Nimavein <michalzygiel00@gmail.com>
2021-09-20 14:57:31 +0200

    history section layout on big screens fix

--- src/components/Subpages/AboutUs/HistorySection/HistorySection.styles.js
+++ src/components/Subpages/AboutUs/HistorySection/HistorySection.styles.js
@@ -14,6 +14,7 @@ export const HistorySectionWrapper = styled.div`
   justify-content: center;
   padding: 0 24px 0 24px;
   margin: 0 auto 148px auto;
+  max-width: 1197px;
 
   @media ${({ theme }) => theme.medias.medium} {
     display: flex;
